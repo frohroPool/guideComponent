8c66e93d9a80cf3fc5d99812a8c9d8f5429b8a92
JORGE ENRIQUE ESPINOSA MEDINA <JEESPINOSAM@Puerto.liverpool.com.mx>
2018-08-28 16:04:51 -0500

    Refactor Header

--- galerias/dev/js/components/finder.js
+++ galerias/dev/js/components/finder.js
@@ -1,18 +1,16 @@
 export const HandlerFinder = (function(){
     
-    //CAMBIA EL ICNO DE BUSQUEDA POR X SI LA CAJA DE BUSQUEDA CONTIENE TEXTO
+    //CAMBIA EL ICONO DE BUSQUEDA POR X SI LA CAJA DE BUSQUEDA CONTIENE TEXTO
     let evtChangeSearchBarIcon = function(){        
         let textBar = document.getElementById("top-search-text");       
         textBar.addEventListener("keyup", function(event){            
             if(textBar.value.length != 0)
-            {   //ELIMINA CLASES PARA OCULTAR BARRA Y AGREGA LAS DE VISUALIZACION
-                document.getElementById("top-search-bar-icon").classList.remove("fa-search");
-                document.getElementById("top-search-bar-icon").classList.add("fa-times");                
+            {   //CAMBIA TEXTO DE MATERIAL ICON
+                document.getElementById("top-search-bar-icon").innerText="close";                
             }
             else
             {
-                document.getElementById("top-search-bar-icon").classList.add("fa-search");
-                document.getElementById("top-search-bar-icon").classList.remove("fa-times");                
+                document.getElementById("top-search-bar-icon").innerText="search";               
             }
         }, false);
     }
